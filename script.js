8dc6e65cf23b4f3be189aebf1566e7842b198812
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-09-28 14:36:13 -0400

    CSS

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-fetch("/JSON/archive.json")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Handle the JSON data here
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch request
-    console.error("Fetch error:", error);
-  });
+fetch('./JSON/archive.json')
+  .then((response)=>response.json())
+  .then((data)=>console.log(data));
 
