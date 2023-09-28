f9fd58fa859378571b7ffe2b9f6d5f2e763aec32
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-09-28 16:49:56 -0400

    data

--- script.js
+++ script.js
@@ -1,22 +1,74 @@
-// var express = require('express');
-// var cors = require('cors');
-// var app = express();
+// async function getData(){
+//   const response = await fetch('./JSON/archive.json');
+//   const data = await response.json();
 
-// app.use(cors({origin: true, credentials: true}));
-  
-// function getData(){
-//   fetch('./JSON/archive.json')           //api for the get request
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+//   console.log(data.Sheet1)
 // }
+// getData();
+
+// function setData() {
+//   document.getElementById("title").textContent = data.Sheet1[0].title;
+// }
+// setData();
+
+// async function getData() {
+//   const response = await fetch('./JSON/archive.json');
+//   const data = await response.json();
+
+//   console.log(data.Sheet1);
+
+
+//   setData(data.Sheet1);
+// }
+
+// function setData(sheetData) {
+//   document.getElementById("title").textContent = sheetData[2].title;
+//   document.getElementById("tag").textContent = sheetData[2].tag;
+// }
+
 
 // getData();
 
-async function getData(){
+async function populateTable() {
   const response = await fetch('./JSON/archive.json');
   const data = await response.json();
+  const dataTable = document.getElementById("dataTable");
 
-  console.log(data.Sheet1[0].tag);
+  // Get the table body where we'll add the rows
+  const tbody = dataTable.querySelector("tbody");
+
+  // Loop through the data and create a row for each item
+  data.Sheet1.forEach(item => {
+    const row = document.createElement("tr");
+
+    // Create table cells for each property in the JSON data
+    const titleCell = document.createElement("td");
+    titleCell.textContent = item.title;
+    row.appendChild(titleCell);
+
+    // const descriptionCell = document.createElement("td");
+    // descriptionCell.textContent = item.description;
+    // row.appendChild(descriptionCell);
+
+    // const imageCell = document.createElement("td");
+    // const image = document.createElement("img");
+    // image.src = item.image;
+    // image.alt = "Image";
+    // imageCell.appendChild(image);
+    // row.appendChild(imageCell);
+
+    // const idCell = document.createElement("td");
+    // idCell.textContent = item.id;
+    // row.appendChild(idCell);
+
+    const tagCell = document.createElement("td");
+    tagCell.textContent = item.tag;
+    row.appendChild(tagCell);
+
+    // Add the row to the table body
+    tbody.appendChild(row);
+  });
 }
 
-getData();
+// Call the function to populate the table
+populateTable();
