90f70e40b06260ac5ee466cce0b946f20968626c
Anna Sham <72109977+annasham@users.noreply.github.com>
2023-09-28 16:08:54 -0400

    data

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-fetch('./JSON/archive.json')
-  .then((response)=>response.json())
-  .then((data)=>console.log(data));
+// var express = require('express');
+// var cors = require('cors');
+// var app = express();
 
+// app.use(cors({origin: true, credentials: true}));
+  
+// function getData(){
+//   fetch('./JSON/archive.json')           //api for the get request
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// }
+
+// getData();
+
+async function getData(){
+  const response = await fetch('./JSON/archive.json');
+  const data = await response.json();
+
+  console.log(data.Sheet1[0].tag);
+}
+
+getData();
